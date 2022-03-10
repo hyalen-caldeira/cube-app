972641cf0272f18460f28a4e9964f23a8e86221d
Hyalen Caldeira <hyalen.moreira@gmail.com>
2022-03-10 12:19:50 -0500

    First commit. Some folders files created.

---
+++ src/test/java/com/cubeapp/cubeapp/CubeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cubeapp.cubeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
